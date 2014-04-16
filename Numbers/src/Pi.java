ba1ad2e5d07cf5e5f11fa839bb87fb38c809ab5a
Jack Lynch <jacklynch@wharnal.com>
2014-04-16 12:35:19 -0400

    completed pi project, renamed scratch readme and updated it

--- Numbers/src/Pi.java
+++ Numbers/src/Pi.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 
 public class Pi {
 	
 	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the number of digits of pi to print:");
+		int digits = Integer.parseInt(input.nextLine());
+		input.close();
 		
+		System.out.printf("pi to %d digits is %." + digits + "f%n", digits, 16 * Math.atan(0.2) - 4 * Math.atan((float) 1 / (float) 239));
 	}
 
 }
